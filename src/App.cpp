aed67afe280169c33f2b97edb340fd244bede372
灵 灵 <482403284@qq.com>
2026-04-18 00:37:24 +0800

    p17

--- src/App.cpp
+++ src/App.cpp
@@ -13,6 +13,7 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "VertexBufferLayout.h"
+#include "Texture.h"
 
 
 int main(void)
@@ -44,10 +45,10 @@ int main(void)
 	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-            -0.5f, -0.5f, //0
-             0.5f, -0.5f, //1  
-             0.5f,  0.5f, //2 
-            -0.5f,  0.5f  //3
+            -0.5f, -0.5f, 0.0f,0.0f,//0
+             0.5f, -0.5f, 1.0f,0.0f,//1  
+             0.5f,  0.5f, 1.0f,1.0f,//2 
+            -0.5f,  0.5f, 0.0f,1.0f //3
         };
 
         unsigned int indices[] = {
@@ -55,11 +56,14 @@ int main(void)
             2, 3, 0
         };
 
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		GLCall(glEnable(GL_BLEND));
 
         VertexArray va;
-        VertexBuffer vb(positions, sizeof(float) * 4 * 2);
+        VertexBuffer vb(positions, sizeof(float) * 4 * 4);
         VertexBufferLayout layout;
         layout.Push<float>(2);
+		layout.Push<float>(2);
         va.AddBuffer(vb,layout);
 
 
@@ -70,6 +74,9 @@ int main(void)
 
         GLCall(shader.Bind());
         GLCall(shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f));
+        Texture texture("rec/textures/ChernoLogo.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 		GLCall(va.Unbind());
         GLCall(shader.Unbind());
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -100,6 +100,12 @@ void Shader::Unbind() const
 	GLCall(glUseProgram(0));
 }
 
+
+void Shader::SetUniform1i(const std::string& name, int value)
+{
+	GLCall(glUniform1i(GetUniformLocation(name), value));
+}
+
 void Shader::SetUniform1f(const std::string& name, float value)
 {
 	GLCall(glUniform1f(GetUniformLocation(name), value));
@@ -110,9 +116,9 @@ void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2,
 	GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
 }
 
-unsigned int Shader::GetUniformLocation(const std::string& name)
+int Shader::GetUniformLocation(const std::string& name)
 {
-	GLCall(unsigned int location = glGetUniformLocation(m_RendererID, name.c_str()));
+	GLCall(int location = glGetUniformLocation(m_RendererID, name.c_str()));
 	if (location == -1)
 		std::cout << "Warning: uniform '" << name << "' doesn't exist!" << std::endl;
 	return location;
