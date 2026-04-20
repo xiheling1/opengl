d45620d04efc50e061dfde7851d6d7849b5c65e7
灵 灵 <482403284@qq.com>
2026-04-20 21:09:49 +0800

    p20投影矩阵

--- src/App.cpp
+++ src/App.cpp
@@ -31,7 +31,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
    
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -48,10 +48,10 @@ int main(void)
 	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-            -0.5f, -0.5f, 0.0f,0.0f,//0
-             0.5f, -0.5f, 1.0f,0.0f,//1  
-             0.5f,  0.5f, 1.0f,1.0f,//2 
-            -0.5f,  0.5f, 0.0f,1.0f //3
+            100.0f, 100.0f, 0.0f,0.0f,//0
+            200.0f, 100.0f, 1.0f,0.0f,//1  
+            200.0f, 200.0f, 1.0f,1.0f,//2 
+            100.0f, 200.0f, 0.0f,1.0f //3
         };
 
         unsigned int indices[] = {
@@ -74,7 +74,9 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::vec4 vp = glm::vec4(100.0f, 100.0f,0.0f,1.0f);
+		glm::vec4 result = proj * vp;
 
 
         Shader shader("rec/Shader/Basic.shader");
