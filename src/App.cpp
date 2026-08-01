64f9f5aa1b4d1311dc7f5db76309e2af7d936fa0
灵 灵 <482403284@qq.com>
2026-08-01 18:59:01 +0800

    p26创建纹理测试

--- src/App.cpp
+++ src/App.cpp
@@ -22,6 +22,7 @@
 #include "imgui/imgui_impl_glfw_gl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void)
 {
@@ -65,6 +66,7 @@ int main(void)
 		currentTest = testMenu;
 
 		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+		testMenu->RegisterTest<test::TestTexture2D>("Texture 2D");
 
 		test::TestClearColor test;
 
